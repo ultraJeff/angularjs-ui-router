8ba1bdccfc8f677f650148719df8d0986d405b6f
ultraJeff <jrobertfranklin@gmail.com>
2015-02-27 10:37:13 -0800

    Final Updates for AngularJS video 1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,15 @@
  * Controller of the codeAcademyApp
  */
 angular.module('codeAcademyApp')
-  .controller('MainCtrl', ['$scope', '$http', 'friends', function ($scope, $http, friends) {
+  .controller('MainCtrl', ['$scope', '$http', 'Friends', function ($scope, $http, Friends) {
       	$scope.title = 'Home';
-      	$scope.friends = friends;
+      	Friends.get().then(function(data) {
+      		$scope.friends = data;
+      	});
       	$scope.items = ['red','green','blue'];
       	$scope.selectedValue = 'red';
 
       	$scope.save = function() {
-      		$http.post('api/friends.json', friends);
+      		$http.post('api/friends.json', Friends);
       	};
   }]);
--- app/scripts/services/friends.js
+++ app/scripts/services/friends.js
@@ -2,15 +2,12 @@
 
 angular
 	.module('codeAcademyApp')
-	.factory('Friends', function() {
+	.factory('Friends', ['$http', function($http) {
 		return {
 			get: function() {
-				return [
-					{'firstName': 'Joe', 'lastName': 'Blow', 'age': 60, 'gender': 'm'},
-					{'firstName': 'Mike', 'lastName': 'Hunt', 'age': 29, 'gender': 'm'},
-					{'firstName': 'Sarah', 'lastName': 'Bildeberg', 'age': 35, 'gender': 'f'},
-					{'firstName': 'Jenny', 'lastName': 'Munt', 'age': 26, 'gender': 'f'}
-				];
+				return $http.get('api/friends.json').then(function(response) {
+					return response.data;
+				});
 			}
 		};
-	});
+	}]);
